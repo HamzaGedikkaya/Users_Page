2eb878edb8f8a88b0ef177a99c0c9b13cc46e626
Hamza Gedikkaya <hamza.gedikkaya@iwallet.com.tr>
2023-09-05 15:09:00 +0300

    User model created

---
+++ db/migrate/20230905120813_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :username
+      t.string :email
+      t.string :address
+      t.string :phone
+
+      t.timestamps
+    end
+  end
+end
