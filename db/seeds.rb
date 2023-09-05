9070902f9005ebc6eec618b5d1cba244306f502b
Hamza Gedikkaya <hamza.gedikkaya@iwallet.com.tr>
2023-09-05 15:18:01 +0300

    Users added to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'httparty'
+
+response = HTTParty.get('https://jsonplaceholder.typicode.com/users')
+users_data = JSON.parse(response.body)
+
+start_id = 1
+
+users_data.each do |user_data|
+  User.create(
+    id: start_id,
+    name: user_data['name'],
+    username: user_data['username'],
+    email: user_data['email'],
+    address: "#{user_data['address']['street']}, #{user_data['address']['suite']}, #{user_data['address']['city']}, #{user_data['address']['zipcode']}",
+    phone: user_data['phone']
+  )
+  start_id += 1
+end
+
+puts 'Kullanıcılar başarıyla eklenmiştir.'
