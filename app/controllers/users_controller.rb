f95f237b2935d675034b3334ce6bdf2e22c8fd85
Hamza Gedikkaya <hamza.gedikkaya@iwallet.com.tr>
2023-09-05 17:50:43 +0300

    Picsum photos added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   # GET /users/1 or /users/1.json
   def show
+    @picsum_url = "https://picsum.photos/id/#{@user.id}/250/166"
   end
 
   # GET /users/new
